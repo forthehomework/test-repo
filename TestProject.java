f088731dbae8e9df06f4085c21aa3dee0288b026
forthehomework <trollyoface19@gmail.com>
2024-05-23 15:34:49 -0400

    project commit test

---
+++ TestProject.java
@@ -0,0 +1,8 @@
+
+public class TestProject {
+    public static void main(String[] args) {
+    String hello = "Hello";
+    String world = "World";
+    System.out.println(String.format("%s %s", hello,world));
+    }
+}
